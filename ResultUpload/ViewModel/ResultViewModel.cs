09a6b82da1521dbd0bd6b91ed3ac817f152550a5
MX1228 <540706265@qq.com>
2019-10-30 14:27:07 +0800

    增加备注

--- ResultUpload/ViewModel/ResultViewModel.cs
+++ ResultUpload/ViewModel/ResultViewModel.cs
@@ -12,8 +12,11 @@ namespace ResultUpload.ViewModel
         public string SName { get; set; }
         //标题
         public string Title { get; set; }
+        //描述
         public string Discription { get; set; }
+        //类别
         public string Type { get; set; }
+        //附件
         public string Attachmet { get; set; }
     }
 }
